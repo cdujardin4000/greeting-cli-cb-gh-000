135a2534e3255a680748e76a4ea5fbdd5a02b0c3
Cedric Dujardin <cedric.dujardin@laposte.net>
2019-12-13 18:28:39 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,4 +1,4 @@
 # code the #greeting method here!
-def greeting(name.string.chomp)
-  puts "Hello #{name}. It's nice to meet you."
+def greeting(name)
+  puts "Hello #{name.string.chomp}. It's nice to meet you."
 end
